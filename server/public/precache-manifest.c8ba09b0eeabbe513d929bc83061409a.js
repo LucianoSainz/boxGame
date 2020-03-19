50f63d8a594f16d30f83d49c9ec53dd4788c5fcc
luciaoSainz <danielsainz28@gmail.com>
2020-03-19 22:41:05 +0100

    deploid

---
+++ server/public/precache-manifest.c8ba09b0eeabbe513d929bc83061409a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b67ec2eaef24eace561c1de1800116ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cefa9172e24f6a7f5625",
+    "url": "/static/css/main.afea1a25.chunk.css"
+  },
+  {
+    "revision": "06bf5a30984afe2c665a",
+    "url": "/static/js/2.1e56b3a4.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.1e56b3a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cefa9172e24f6a7f5625",
+    "url": "/static/js/main.bb0a39a4.chunk.js"
+  },
+  {
+    "revision": "cda345fb50b7e11c0025",
+    "url": "/static/js/runtime-main.844668cf.js"
+  },
+  {
+    "revision": "ff02283c76eefca1d9dcec3b74a1a39c",
+    "url": "/static/media/logo.ff02283c.png"
+  },
+  {
+    "revision": "f3bf974799df82e5c5b8cc3609ce2f52",
+    "url": "/static/media/logo_transparent.f3bf9747.png"
+  }
+]);
